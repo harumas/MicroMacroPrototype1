b1cda58760881ade57b923e1510af660cf8df5c5
harumas <haruto09.game@gmail.com>
2025-04-22 18:59:50 +0900

    調整

--- Assets/Scripts/Gimmick/Balloon.cs
+++ Assets/Scripts/Gimmick/Balloon.cs
@@ -11,27 +11,44 @@ namespace Gimmick
         [SerializeField] private float unscaleSpeed = 0.1f;
         [SerializeField] private CatchableObject catchableObject;
         [SerializeField] private Rigidbody rig;
+        [SerializeField] private Transform lookTransform;
 
         private float antiGravityPower;
+        private bool wasShoot;
         private Vector3 defaultScale;
+        private Vector3 lookDirection;
 
         private void Start()
         {
             catchableObject.OnRelease += OnRelease;
+            catchableObject.OnShoot += OnShoot;
             defaultScale = transform.localScale;
         }
 
         private void FixedUpdate()
         {
-            rig.AddForce(Vector3.up * antiGravityPower);
+            rig.AddForce(lookDirection * antiGravityPower);
 
             transform.localScale = Vector3.Lerp(transform.localScale, defaultScale, unscaleSpeed * Time.fixedDeltaTime);
             antiGravityPower = transform.localScale.x * antiGravityScale;
         }
 
+        private void OnShoot(Vector2 direction)
+        {
+            lookDirection = Vector3.up;
+            wasShoot = true;
+        }
+
         private void OnRelease(float scale)
         {
             antiGravityPower = scale * antiGravityScale;
+
+            if (!wasShoot)
+            {
+                lookDirection = lookTransform.right;
+            }
+
+            wasShoot = false;
         }
     }
 }
